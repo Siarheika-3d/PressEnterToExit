b0e597fe54a7f95a3b97ee544fb69e4e5c83a3a4
Serge P <serge.prokhorov@hypernovateam.com>
2023-08-10 23:16:33 +0200

    Add project files.

---
+++ PressEnterToExit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PressEnterToExit
+{
+
+    /// <summary>
+    /// Somehow the class name should reflect purpose or the main meaning of code of the class. 
+    /// This is a try to make a text line only with "Console.Write" method and close the console with "Enter" button.
+    /// </summary>
+    class ConsoleClose
+    {
+        /// <summary>
+        /// Main part of executable code. It checks if the pressed key is not equal to the Enter key. 
+        /// Once the user presses the Enter key, the condition becomes false, and the check loop exits closing the console window.
+        /// </summary>
+        /// <param name="args">Three words with "Console.Write" method and text of exit describtion on the next line.</param>
+        static void Main(string[] args)
+               {
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!\n\n");
+            Console.WriteLine("Press Enter to close the console...");
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter) { }
+        }
+    }
+}
